5567f49df1626ec23191512686f446c06404e849
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-05-03 22:01:18 +0200

    made productcard component and a responsive grid for them to live in. Also implemented smart serach-function into Products.jsx

--- src/components/productComponents/ProductCard.jsx
+++ src/components/productComponents/ProductCard.jsx
@@ -2,13 +2,52 @@ import styled from "styled-components"
 
 
 const Card = styled.div`
-	background-color: pink;
+	padding: 1em;
+	border-radius:1em;
+	display: flex;
+	flex-direction:column;
+	
+	@media (min-width: 400px) {
+		box-shadow: 0.3em 0.3em 1em lightgray;
+	}  
+
+	@media (min-width: 1000px) {
+	&:hover {
+		box-shadow: 0.2em 0.2em 0.6em lightgray;
+		transform: scale(1.075)
+	}
+}
 `
 
-const ProductCard = ({ name, price }) => (
+const CardImg = styled.img`
+	width: 100%;
+	aspect-ratio: 1/1;
+	object-fit: cover;
+
+`
+
+const CardContentContainer = styled.div`
+	padding: 1em;
+	display: flex;
+	flex-direction:column;
+	justify-content: space-between;
+	flex-grow: 1;
+`
+
+const CardPrice = styled.p`
+	align-self: flex-end;
+	margin-top: 0.5em;
+	font-weight: bold;
+	font-size: 1.2em;
+`
+
+const ProductCard = ({ name, price, img }) => (
 	<Card>
-		<h3>{name}</h3>
-		<p>{price}:-</p>
+		<CardImg src={img[0]} alt={name} />
+		<CardContentContainer>
+			<h3>{name}</h3>
+			<CardPrice>{price}:-</CardPrice>
+		</CardContentContainer>
 	</Card>
 )
 
--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,16 +1,65 @@
 import styled from "styled-components";
 import ProductCard from "../components/productComponents/ProductCard";
 import testData from "../data/testdata";
+import { useState } from "react";
 
 
-const Products = () => (
-	<div>
-		<h1>Products</h1>
-		{testData.map(product => (
-			<ProductCard name={product.name} price={product.price} />
-		))}
-	</div>
-)
+const ProductGrid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(9rem, 1fr));
+	@media (min-width: 400px) {
+    gap: 1em;
+	grid-template-columns: repeat(auto-fit, minmax(11rem, 1fr));
+  }
+	@media (min-width: 770px) {
+		padding: 3em;
+		grid-template-columns: repeat(auto-fit, minmax(13rem, 1fr));
+	}
+	
+	@media (min-width: 1000px) {
+		padding: 3em 5em;
+		gap:2em;
+	grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  }
+`
+
+function stringIncludes(longString, shortString) {
+	let ls = longString.toLowerCase()
+	let ss = shortString.toLowerCase()
+	return ls.includes(ss)
+}
+function productMatch(product, searchString) {
+	if (stringIncludes(product.name, searchString)) {
+		return true
+	} else {
+		return false
+	}
+}
+
+const Products = () => {
+	const [matches, setMatches] = useState(testData)
+
+	const handleChange = event => {
+		let searchString = event.target.value
+		setMatches(testData.filter(product => productMatch(product, searchString)))
+	}
+
+	return (
+		<div>
+			<h1>Products</h1>
+			<input
+				type="text"
+				placeholder="Sök efter produkt.."
+				onChange={handleChange} />
+			<ProductGrid>
+				{matches.map(({ productid, name, price, picture }) => (
+					<ProductCard key={productid} name={name} price={price} img={picture} />
+				))}
+			</ProductGrid>
+		</div>
+	)
+}
+
 
 
 export default Products
